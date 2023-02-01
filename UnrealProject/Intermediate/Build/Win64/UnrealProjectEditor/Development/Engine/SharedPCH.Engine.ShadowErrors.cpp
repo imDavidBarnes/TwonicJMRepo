9954907ba99233183d8c7722a39783a025ce5c2d
Patertick <72743563+Patertick@users.noreply.github.com>
2023-02-01 21:51:53 +0000

    Added stamina bar
    Improved stamina bar to look more like tunic.
    Merged starting projects from designers and programmers.
    Features include:
    -Isometric viewpoint
    -Sprinting
    -Stamina metre

--- UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.32.31329
-#include "C:/Users/B1035382/Documents/GitHub/TwonicJMRepo/UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+// Compiler: 14.29.30038
+#include "D:/JourneyManProject/GitHub/TwonicJMPatrick/TwonicJMRepo/UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
