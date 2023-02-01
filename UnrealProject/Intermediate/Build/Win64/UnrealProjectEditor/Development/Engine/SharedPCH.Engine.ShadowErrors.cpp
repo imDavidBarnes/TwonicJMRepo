1edb32f76dfa2608c9215941aab0b70fabe125b6
Patertick <72743563+Patertick@users.noreply.github.com>
2023-02-01 19:03:00 +0000

    Merged programmer and designer starting projects
    Dragged all the content files from the third person base project, to change which character blueprint is spawning, change the game mode to corresponding game mode blueprint. If any errors occur refer to a backup and msg me for the required (isometric and sprinting) files.

--- UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.32.31329
-#include "C:/Users/B1035382/Documents/GitHub/TwonicJMRepo/UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+// Compiler: 14.29.30038
+#include "C:/Users/patri/OneDrive/Documents/GitHub/TwonicJMPatrick/UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
