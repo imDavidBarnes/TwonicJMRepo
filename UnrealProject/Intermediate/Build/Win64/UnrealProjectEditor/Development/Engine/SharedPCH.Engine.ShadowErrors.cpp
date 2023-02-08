156f73754ebd044fbd3a223c3f437db21e829309
DrewL101 <b1092998@live.tees.ac.uk>
2023-02-08 17:13:26 +0000

    LockOnSystem
    Made a lock on system for the enemys also added enemy datatable and Main Enemy BP so it can work with lock system

--- UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.32.31329
-#include "C:/Users/B1035382/Documents/GitHub/TwonicJMRepo/UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+// Compiler: 14.34.31937
+#include "D:/OneDrive - Teesside University/TwonicJMRepo/UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
