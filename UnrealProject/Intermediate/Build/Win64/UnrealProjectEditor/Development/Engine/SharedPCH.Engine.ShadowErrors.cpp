80dd887ea1fcf6273d01bf8e542da4a42cd6a672
Mystic <64146072+Dragonness@users.noreply.github.com>
2023-03-01 13:10:31 +0000

    Inventory n Chara Anim
    Started on the inventory - did the HUD so i won't miss anything and the code itself - and then started trying to figure out character animations so we can add them by the end of the week ready for game testing.

--- UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.29.30038
-#include "D:/JourneyManProject/TwonicJMRepo/UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+// Compiler: 14.32.31329
+#include "F:/1. School/3. Uni Stuff/Journeyman/TwonicJMRepo/UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
