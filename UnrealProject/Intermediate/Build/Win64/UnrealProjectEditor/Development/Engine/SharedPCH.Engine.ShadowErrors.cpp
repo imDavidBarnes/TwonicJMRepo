7236c35e6230f1d9d7f82acc820596965ab34679
Patertick <72743563+Patertick@users.noreply.github.com>
2023-03-03 14:28:16 +0000

    Added enemy type
    New enemy type BP_BouncingCactus. It rolls towards the player when it sees them. Normally just stays still since I thought it was supposed to blend in with the environment? Looks weird at the moment since the skeletal mesh isn't round.

--- UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.35.32215
-#include "C:/Users/david/Documents/GitHub/TwonicJMRepo/UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+// Compiler: 14.29.30038
+#include "D:/JourneyManProject/TwonicJMRepo/UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
