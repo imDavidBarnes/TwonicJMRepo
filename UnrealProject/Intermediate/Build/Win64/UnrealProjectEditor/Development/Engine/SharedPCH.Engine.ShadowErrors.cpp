038d964d4dee71f94a0379f42a30d143854b9fa4
DrewL101 <b1092998@live.tees.ac.uk>
2023-03-21 18:54:38 +0000

    Fixed SwitchLockOnBug Also Added Icons to enemy huds when hit with bombs

--- UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
 // Compiler: 14.35.32215
-#include "C:/Users/david/Documents/GitHub/TwonicJMRepo/UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+#include "C:/Users/drew_/OneDrive/Desktop/TwonicJMRepo/UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
