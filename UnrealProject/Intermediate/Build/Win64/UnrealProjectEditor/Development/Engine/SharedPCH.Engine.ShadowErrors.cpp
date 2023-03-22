50f1e4c1acf18dd7447ac29a77ccf07b910478e8
DrewL101 <b1092998@live.tees.ac.uk>
2023-03-22 13:15:17 +0000

    Added some changes to names and some fixes

--- UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,6 @@
 // Compiler: 14.32.31329
+<<<<<<< Updated upstream
 #include "C:/Users/V8181926/Documents/GitHub/TwonicJMRepo/UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+=======
+#include "C:/Users/b1092998/Desktop/TwonicJMRepo/UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+>>>>>>> Stashed changes
